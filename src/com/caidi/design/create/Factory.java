738bc09500c45533f30459f6ff455679bf522d8e
caidi <caidif@163.com>
2020-10-13 17:28:15 +0800

    设计模式1.0

---
+++ src/com/caidi/design/create/Factory.java
@@ -0,0 +1,10 @@
+package com.caidi.design.create;
+
+/**
+ * @author: 创建型
+ * @date: 15:29 2020/10/13
+ * @description:
+ */
+public class Factory {
+
+}
---
+++ src/com/caidi/design/create/Singleton.java
@@ -0,0 +1,10 @@
+package com.caidi.design.create;
+
+/**
+ * @author: 蔡迪
+ * @date: 15:34 2020/10/13
+ * @description: 单例
+ */
+public class Singleton {
+
+}
